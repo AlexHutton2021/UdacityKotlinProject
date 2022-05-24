f7b8d0d52fea215c3526bd4b2a12d2ab97e87d43
AlexHutton2021 <94877127+AlexHutton2021@users.noreply.github.com>
2022-05-24 15:45:05 +1000

    Update naming convention of package names

--- src/main/kotlin/oop/Classes.kt
+++
@@ -1,8 +0,0 @@
-package oop
-fun main(args: Array<String>) {
-//Create our own data type
-//keep data members and methods together in one place
-//Write more readable and maintainable code
-
-
-}
--- src/main/kotlin/oop/ex02.kt
+++ src/main/kotlin/oop/ex02.kt
@@ -5,5 +5,16 @@ fun main(args: Array<String>) {
     var age = 17
     if(age>=21)
     println("you may vote now")
+
+    var weight = "68kg"
+    if (weight<="68kg")
+        println("You are in lightweight division")
+
+    var temp = 150
+    if (temp>=150)
+        println("Cake is likely to burn")
+
+
+
 }
 
